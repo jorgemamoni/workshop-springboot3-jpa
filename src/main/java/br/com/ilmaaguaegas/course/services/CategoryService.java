1c5f7a14f97acd4f71a07f086c5f459d06ccaa76
Jorge Mamoni <jmamonip@gmail.com>
2024-02-06 13:56:14 -0300

    Category

---
+++ src/main/java/br/com/ilmaaguaegas/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package br.com.ilmaaguaegas.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.ilmaaguaegas.course.entities.Category;
+import br.com.ilmaaguaegas.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
