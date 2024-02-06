aaf9ead1cbd1856ca31f346342a0ffca83c2561d
Jorge Mamoni <jmamonip@gmail.com>
2024-02-06 10:24:16 -0300

    Service layer, component registration

---
+++ src/main/java/br/com/ilmaaguaegas/course/services/UserService.java
@@ -0,0 +1,26 @@
+package br.com.ilmaaguaegas.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.ilmaaguaegas.course.entities.User;
+import br.com.ilmaaguaegas.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
